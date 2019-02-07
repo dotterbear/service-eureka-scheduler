b2ecfe48179e283ad5116b0f3035aba08cee6651
pqab.annie <pqab.annie@gmail.com>
2019-02-07 23:50:00 +0800

    temp fix

--- src/test/java/dotterbear/service/eureka/scheduler/ServiceSchedulerApplicationTests.java
+++ src/test/java/dotterbear/service/eureka/scheduler/ServiceSchedulerApplicationTests.java
@@ -2,17 +2,12 @@ package dotterbear.service.eureka.scheduler;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ServiceSchedulerApplicationTests {
-
-	@MockBean
-	private ApplicationReadyEvent applicationReadyEvent;
 
 	@Test
 	public void contextLoads() {
